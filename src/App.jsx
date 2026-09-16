7e1d8aaf88cc77d8b56ac3ab32bdc42b1ed9e541
eberriphath <riphatheber@gmail.com>
2026-09-16 15:35:45 +0300

    redirecting issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 import PackageDetails from "./pages/PackageDetails";
@@ -98,6 +98,11 @@ function App() {
         <Route
          path="/privacy-policy"
          element={<PrivacyPolicy />}
+        />
+
+        <Route
+          path="*"
+          element={<Navigate to="/" />}
         />
 
       </Routes>
